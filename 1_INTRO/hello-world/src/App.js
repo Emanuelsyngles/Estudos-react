0c1b6155ea19b4592896db283086fb2481d93869
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-06-18 19:18:19 -0300

    Finalizando a parte de introdução e partindo para adicionar tecnologias mais modernas nas aplicações.

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world React!</h1>
+      <h2>Emmet funcionou!</h2>
     </div>
   );
 }
