d59ce5e4a16e0edbe59e291666d24fb94ed037da
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-06-19 14:02:59 -0300

    Update TemplateExpression.jsx

--- 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpression.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpression.jsx
@@ -1,9 +1,16 @@
 const TemplateExpression = () => {
   const name = "Luan";
+  const data = {
+    age: 28,
+    job: "Programmer",
+  };
 
   return (
     <>
       <h1>Olá {name}, tudo bem?</h1>
+      <p>Você atua como: {data.job}</p>
+      <p>{4 + 4}</p>
+      <p>{console.log("JSX React")}</p>
     </>
   );
 };
