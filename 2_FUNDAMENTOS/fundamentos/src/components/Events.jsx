a2282deeaad82ce79ead8a78c108ad4df268c4e3
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-06-22 21:21:19 -0300

    Update Events.jsx

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -5,6 +5,14 @@ const Events = () => {
     console.log("Ativou o evento!");
   };
 
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando isso!</h1>;
+    } else {
+      return <h1>Também posso renderizar isso!</h1>;
+    }
+  };
+
   return (
     <>
       <>
@@ -24,6 +32,8 @@ const Events = () => {
           Clica aqui, por favor
         </button>
       </>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </>
   );
 };
