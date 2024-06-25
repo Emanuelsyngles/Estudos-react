79efc0ac753bbc60c2053f22c9ee0272817dbe97
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-06-25 15:14:14 -0300

    Renomeando pasta.

---
+++ 3_AVANCANDO_EM_REACT/projeto/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Avançando em React</h1>
+      {/*Imagem em public*/}
+      <>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </>
+    </>
+  );
+}
+
+export default App;
